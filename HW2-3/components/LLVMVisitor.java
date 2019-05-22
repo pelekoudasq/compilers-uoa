c6152abe0f72efed6dd29cdabf4c739dc2ec7dcb
Ioannis Pelekoudas <peleioannis@gmail.com>
2019-05-22 18:48:04 +0300

    buffer added, one file write

--- HW2-3/components/LLVMVisitor.java
+++ HW2-3/components/LLVMVisitor.java
@@ -10,6 +10,7 @@ public class LLVMVisitor extends GJNoArguDepthFirst<String> {
 
 	SymbolTable table;
 	Path file;
+	String buffer;
 	// Class currentClass;
 	// Method currentMethod;
 	// boolean inMethod;
@@ -29,6 +30,7 @@ public class LLVMVisitor extends GJNoArguDepthFirst<String> {
 
 	public LLVMVisitor(SymbolTable table) {
 		this.table = table;
+		this.buffer = "";
 		String llname = "./" + this.table.filename + ".ll";
 		this.file = Paths.get(llname);
 		
@@ -36,33 +38,16 @@ public class LLVMVisitor extends GJNoArguDepthFirst<String> {
 		for(int i = 0; i < keys.size(); i++) {
 			Class tempClass = this.table.cls.get(keys.get(i));
 			if ( this.table.filename.equals(keys.get(i)) ) {
-				try {
-					this.emit("@." + tempClass.name + "_vtable = global [0 x i8*] []\n");
-					continue;
-				} catch(IOException ex) {
-					System.out.println(ex.getMessage());
-				}
+				this.buffer = this.buffer + "@." + tempClass.name + "_vtable = global [0 x i8*] []\n";
+				continue;
 			}
-			try {
-				this.emit("@." + tempClass.name + "_vtable = global [" + tempClass.methods.size() + " x i8*] [");
-			} catch(IOException ex) {
-				System.out.println(ex.getMessage());
-			}
+			this.buffer = this.buffer + "@." + tempClass.name + "_vtable = global [" + tempClass.methods.size() + " x i8*] [";
 			boolean first = true;
 			for (Method m : tempClass.methods) {
-				if (!first) {
-					try {
-						this.emit(", ");
-					} catch(IOException ex) {
-						System.out.println(ex.getMessage());
-					}
-				}
+				if (!first)
+					this.buffer = this.buffer + ", ";
 				String retType = typeJavaToLLVM(m.type);
-				try {
-					this.emit("i8* bitcast (" + retType + " (i8*");
-				} catch(IOException ex) {
-					System.out.println(ex.getMessage());
-				}
+				this.buffer = this.buffer + "i8* bitcast (" + retType + " (i8*";
 				String[] argsSignature = m.getSignature().split(",");
 				String typeArg;
 				for (int j = 0; j < argsSignature.length; j++) {
@@ -70,34 +55,26 @@ public class LLVMVisitor extends GJNoArguDepthFirst<String> {
 					if ( argsSignature[j] == null || argsSignature[j].isEmpty() )
 						continue;
 					typeArg = typeJavaToLLVM(argsSignature[j]);
-					try {
-						this.emit(","+typeArg);
-					} catch(IOException ex) {
-						System.out.println(ex.getMessage());
-					}
+					this.buffer = this.buffer + "," + typeArg;
 				}
-				try {
-					this.emit(")* @" + tempClass.name + "." + m.name + " to i8*)");
-				} catch(IOException ex) {
-					System.out.println(ex.getMessage());
-				}
-				
+				this.buffer = this.buffer + ")* @" + tempClass.name + "." + m.name + " to i8*)";
 				first = false;
 			}
-			
-			try {
-				this.emit("]\n");
-			} catch(IOException ex) {
-				System.out.println(ex.getMessage());
-			}
+			this.buffer = this.buffer + "]\n";
 		}
+		this.buffer = this.buffer + "\n\ndeclare i8* @calloc(i32, i32)\ndeclare i32 @printf(i8*, ...)\ndeclare void @exit(i32)\n\n@_cint = constant [4 x i8] c\"%d\\0a\\00\"\n@_cOOB = constant [15 x i8] c\"Out of bounds\\0a\\00\"\ndefine void @print_int(i32 %i) {\n\t%_str = bitcast [4 x i8]* @_cint to i8*\n\tcall i32 (i8*, ...) @printf(i8* %_str, i32 %i)\n\tret void\n}\n\ndefine void @throw_oob() {\n\t%_str = bitcast [15 x i8]* @_cOOB to i8*\n\tcall i32 (i8*, ...) @printf(i8* %_str)\n\tcall void @exit(i32 1)\n\tret void\n}\n\n";
+	}
 
+	public String visit(Goal n) {
+		n.f0.accept(this);
+		n.f1.accept(this);
+		n.f2.accept(this);
 		try {
-			this.emit("\n\ndeclare i8* @calloc(i32, i32)\ndeclare i32 @printf(i8*, ...)\ndeclare void @exit(i32)\n\n@_cint = constant [4 x i8] c\"%d\\0a\\00\"\n@_cOOB = constant [15 x i8] c\"Out of bounds\\0a\\00\"\ndefine void @print_int(i32 %i) {\n\t%_str = bitcast [4 x i8]* @_cint to i8*\n\tcall i32 (i8*, ...) @printf(i8* %_str, i32 %i)\n\tret void\n}\n\ndefine void @throw_oob() {\n\t%_str = bitcast [15 x i8]* @_cOOB to i8*\n\tcall i32 (i8*, ...) @printf(i8* %_str)\n\tcall void @exit(i32 1)\n\tret void\n}\n\n");
+			this.emit(this.buffer);
 		} catch(IOException ex) {
 			System.out.println(ex.getMessage());
 		}
-
+		return null;
 	}
 
 	public String visit(MainClass n) {
